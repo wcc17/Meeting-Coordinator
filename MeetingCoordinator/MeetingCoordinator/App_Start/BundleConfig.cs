a966413bac4b2f8e23647e5b0b6e103e7a60a96e
Wes Gilleland <wes.gilleland@gmail.com>
2016-02-28 21:51:13 -0500

    Initial commit of WebArch theme to the master layout. Next step is to strip out everything not needed

---
+++ MeetingCoordinator/MeetingCoordinator/App_Start/BundleConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MeetingCoordinator
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/plugins/bootstrapv3/css/bootstrap.min.css",
+                      "~/Scripts/plugins/bootstrapv3/css/bootstrap-theme.min.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/theme").Include(
+                      "~/Scripts/plugins/pace/pace-theme-flash.css",
+                      "~/Scripts/plugins/jquery-scrollbar/jquery.scrollbar.css",
+                      "~/Scripts/plugins/font-awesome/css/font-awesome.css",
+                      "~/Scripts/css/animate.min.css",
+                      "~/Scripts/css/style.css",
+                      "~/Scripts/css/responsive.css",
+                      "~/Scripts/css/custom-icon-set.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
+                      "~/Scripts/plugins/jquery-1.8.3.min.js",
+                      "~/Scripts/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js",
+                      "~/Scripts/plugins/bootstrapv3/js/bootstrap.min.js",
+                      "~/Scripts/plugins/breakpoints.js",
+                      "~/Scripts/plugins/jquery-unveil/jquery.unveil.min.js",
+                      "~/Scripts/plugins/jquery-block-ui/jqueryblockui.js",
+                      "~/Scripts/plugins/jquery-scrollbar.min.js",
+                      "~/Scripts/plugins/pace/pace.min.js",
+                      "~/Scripts/plugins/jquery-numberAnimate/jquery.animateNumbers.js"));
+        }
+    }
+}
---
+++ MeetingCoordinator/MeetingCoordinator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeetingCoordinator.Startup))]
+namespace MeetingCoordinator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
